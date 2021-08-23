1fb17e69d62401c96559559a789abc1a30cd8faf
PKrithi <89425822+PKrithi@users.noreply.github.com>
2021-08-23 17:39:36 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var ball;
+
+function setup() {
+  createCanvas(400,400);
+
+  ball = createSprite(200,200,20,20);
+  ball.shapeColor = "green";
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyDown(UP_ARROW)){
+    ball.y = ball.y -2;
+  }
+
+  if(keyDown(DOWN_ARROW)){
+    ball.y = ball.y +2;
+  }
+
+  if(keyDown(RIGHT_ARROW)){
+    ball.x = ball.x +2;
+  }
+
+  if(keyDown(LEFT_ARROW)){
+    ball.x = ball.x -2;
+  }
+  drawSprites();
+
+}
+
+
+
+
